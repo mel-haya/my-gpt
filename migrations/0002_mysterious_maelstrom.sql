275547012f1e179a270e7d44b61ff809d35ebfae
mel-haya <yas20072003@gmail.com>
2025-11-19 01:01:00 +0100

    feat: Update database schema for messages and conversations, including foreign key constraints and new roles enum

---
+++ migrations/0002_mysterious_maelstrom.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "messages" ADD CONSTRAINT "messages_conversation_id_conversations_id_fk" FOREIGN KEY ("conversation_id") REFERENCES "public"."conversations"("id") ON DELETE no action ON UPDATE no action;
