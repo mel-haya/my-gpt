22ee260ee7074b7ae605202554053b73f2df885b
mel-haya <yas20072003@gmail.com>
2025-12-30 11:56:37 +0100

    fix: admin page doesn't scroll sidebar

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -10,9 +10,11 @@ export default function AdminLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex">
+    <div className="flex h-screen">
       <Sidebar />
-      {children}
+      <div className="flex-1 overflow-y-auto">
+        {children}
+      </div>
       <ToastContainer autoClose={3000} theme="dark" pauseOnHover={false} />
     </div>
   );
